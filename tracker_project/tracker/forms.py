86adf105a2f3f698115463fcbb349e2a0b0ae41e
Nico Morell <nicomorell@outlook.com>
2018-09-22 14:06:22 +0100

    Added Placeholders

--- tracker_project/tracker/forms.py
+++ tracker_project/tracker/forms.py
@@ -8,10 +8,14 @@ class BMIForm(forms.ModelForm):
         fields = ('age', 'heightFeet', 'heightMetres', 'heightInches', 'weightPounds', 'weightKilos')
 
 class weightForm(forms.ModelForm):
-    prevWeight = forms.FloatField(required = False)
+    prevWeight = forms.FloatField(required = False, label = 'prevWEight')
     afterWeight = forms.FloatField(required=False)
     prevDay = forms.IntegerField(required = False)
     afterDay = forms.IntegerField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(weightForm, self).__init__(*args, **kwargs)
+        self.fields['prevWeight'].widget.attrs['placeholder'] = 'Previous Weight'
     class Meta:
         model = Weight
         fields = ('mass', 'day', 'month', 'year')
